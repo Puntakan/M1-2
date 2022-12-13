fe14a1ac5651c905f65cb40e48b06f5d15c2cf6f
Puntakan <88113836+Puntakan@users.noreply.github.com>
2022-12-13 23:02:23 +0700

    Update getItemsOfCurrentPage.js

--- getItemsOfCurrentPage.js
+++ getItemsOfCurrentPage.js
@@ -1,6 +1,13 @@
 const { template } = require('@babel/core')
 //write your student id, firstname, and lastname herefunction getItemsOfCurrentPage(allItems, currentPage, rowsPerPage) {
- if(allItems===null ||allItems===undefined)return undefined if(allItems.length===0)return []
- if(rowsPerPage<=0)return allItems if(currentPage<=0)return allItems const itemsOnCurrentPage=allItems.slice(rowsPerPage*currentPage-rowsPerPage, rowsPerPage*currentPage)
+ if(allItems===null ||allItems===undefined)
+  return undefined 
+if(allItems.length===0)
+ return []
+ if(rowsPerPage<=0)
+  return allItems 
+if(currentPage<=0)
+ return allItems 
+const itemsOnCurrentPage=allItems.slice(rowsPerPage*currentPage-rowsPerPage, rowsPerPage*currentPage)
  return itemsOnCurrentPage.length===0?[]:itemsOnCurrentPage}
 module.exports = getItemsOfCurrentPage
